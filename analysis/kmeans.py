26194280dbee9e0e6e30ad676b6d283059166f6a
Scott Harvey <bundito@gmail.com>
2018-03-15 17:06:03 -0500

    All files analyzed & stored in database

--- analysis/kmeans.py
+++ analysis/kmeans.py
@@ -6,7 +6,7 @@ from sklearn.cluster import KMeans
 import cv2
 import numpy as np
 import time
-
+import sqlite3
 
 def rgb2hsv(r, g, b):
     r, g, b = r / 255.0, g / 255.0, b / 255.0
@@ -39,11 +39,16 @@ def centroid_histogram(clusters):
 	# return the histogram
 	return hist
 
+conn = sqlite3.connect("/home/bundito/projects/PforP/pforp.db")
+c = conn.cursor()
+
 starttime = time.time()
 
 print("\nStarting run at {}\n" . format(time.asctime()))
 
 workdir = sys.argv[1]
+volnum = sys.argv[2]
+
 filelist = os.listdir(workdir)
 
 imagecount = 1
@@ -82,7 +87,6 @@ for image in filelist:
 
     colors.sort(reverse=True)
 
-
     for color in colors:
         percent, r, g, b, h, s, v = color
         print("Percent {} / ColorRGB {} {} {} / ColorHSV {} {} {}".format(percent, r, g, b, h, s, v))
@@ -90,6 +94,21 @@ for image in filelist:
 
     imagecount = imagecount + 1
 
+    c1 = colors[0]
+    c2 = colors[1]
+    c3 = colors[2]
+
+    #                              pct     r      g     b      h      s       v
+    imgdata = (imagename, volnum, c1[0], c1[1], c1[2], c1[3], c1[4], c1[5], c1[6],
+                                  c2[0], c1[1], c1[2], c2[3], c2[4], c2[5], c2[6],
+                                  c3[0], c3[1], c3[2], c3[3], c3[4], c3[5], c3[6])
+    #print(imgdata)
+
+    c.execute('INSERT INTO images VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', imgdata)
+    conn.commit()
+
+c.close()
+conn.close()
 endtime = time.time()
 elapsed = round((endtime - starttime), 2)
 print("Done.")
