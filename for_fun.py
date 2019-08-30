bc9e2940981136b23afdcc31a9fe44f55eb2597c
Scott Harvey <bundito@gmail.com>
2019-08-30 13:54:45 -0500

    Still refactoring!

--- for_fun.py
+++ for_fun.py
@@ -6,16 +6,35 @@ import sys
 import sqlite3
 
 from PySide2.QtWidgets import (QLineEdit, QPushButton, QApplication, QVBoxLayout, QDialog)
+from PySide2.QtGui import (QPoint, QPolygon)
+
+import random
 
 import cv2            
 import numpy as np
 
 image = cv2.imread("gaussian.jpg")
 
-px = image[50,75]
-print(px)
+i = 0
+all_b = 0
+all_g = 0
+all_r = 0
 
+while i < 5:
+    rnd_x = random.randint(0,511)
+    rnd_y = random.randint(0,511)
 
+    px = image[rnd_x, rnd_y]
+
+    all_b += px[0]
+    all_g += px[1]
+    all_r += px[2]
+
+    i += 1
+
+print("R: ", all_r / 5)
+print("G: ", all_g / 5)
+print("B: ", all_b / 5) 
 
 """
 try:
@@ -39,3 +58,23 @@ finally:
         print("The SQLite connection is closed")
  """
 
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        
+
+        self.polygon = QPolygon()
+        self.polygon << QPoint(10, 20) << QPoint(20, 30)
+
+        self.layout = QWidgets.QVBoxLayout
+        self.layout.addWidget(polygon)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    widget = MyWidget()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec_())
