5f46ec8169333b54a4cded185a0d8a6ecf5a455f
Scott Harvey <bundito@gmail.com>
2019-08-31 15:53:58 -0500

    cv error?

--- .idea/man_is_meat.py
+++ .idea/man_is_meat.py
@@ -17,12 +17,12 @@ def list_dirs(path):
         return family """
 
 def gaussify_img(img):
-        work_file = cv2.imread("../img")
-        blur = cv2.blur(work_file,(5,5))
 
-        plt.subplot(121),plt.imshow(work_file),plt.title('Original')
+        blur = cv2.GaussianBlur(img,(5,5),0)
+
+        plt.subplot(121),plt.imshow(img),plt.title('Original')
         plt.xticks([]), plt.yticks([])
-        plt.subplot(122),plt.imshow(work_file),plt.title('Blurred')
+        plt.subplot(122),plt.imshow(img),plt.title('Blurred')
         plt.xticks([]), plt.yticks([])
         plt.show()
 
@@ -30,26 +30,23 @@ def gaussify_img(img):
 
 # FUNCTIONS ARE NOW EXTINCT - MAIN CODE BELOW
 
-# LEVEL THE FIRST -- DIRECTORIES OF IMAGES
-
-dir_list = list_dirs("Propaganda-JPEGs/Vol1")
+# LEVEL THE FIRST -- DIRECTORIES OF IMAGE
 
 
 
 # for work_dir in dir_list:
 #         print(work_dir)
 
-print(dir_list)
+work_dir = "Propaganda-JPEGs/Vol1"
+work_files = os.listdir(work_dir)
 
-#file_list = file_list(work_dir)
-
-for process_file in dir_list:
+for process_file in work_files:
         """ family = get_family(process_file)
         print(family)
  """
         print(process_file)
 
-        #gaussed_file = gaussify_img(process_file)
+        gaussed_file = gaussify_img(process_file)
 
 
 
