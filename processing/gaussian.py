02560449bf4f0a7839135a0fac48d7b6bf15d369
Scott Harvey <bundito@gmail.com>
2019-09-05 22:06:30 -0500

    House = Cleaned

--- processing/gaussian.py
+++
@@ -1,8 +0,0 @@
-from wand.image import Image
-
-with Image(filename="Add-N-To-X-1.JPG") as original:
-    with original.clone as converted:
-        converted.format = "jpg"
-
-        converted.save(filename="converted.jpg")
-
--- volumesort/Propaganda-indices/mktars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p dist
-
-for vol in Vol* Goodies Propaganda-For-E ; do
-	tar Icf dist/Propaganda-${vol}.tar.bz2 $vol
-done
